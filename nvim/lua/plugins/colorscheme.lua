9b9a316f784d8ab6baff39a7d36eeaa67415fd6b
CrazyCatZhang <crazycatzhang@outlook.com>
2024-01-14 21:24:06 +0800

    Uptdate colorscheme config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,76 @@
+local priority = {
+  LOW = 100,
+  MEDIUM = 200,
+  HIGH = 615,
+}
+
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "Luxed/ayu-vim",
+    priority = priority.HIGH,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    priority = priority.HIGH,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    priority = priority.HIGH,
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = priority.HIGH,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
