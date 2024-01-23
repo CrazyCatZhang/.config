b62f93405ca06d761db4255084f6f2c725e32f8e
CrazyCatZhang <crazycatzhang@outlook.com>
2024-01-23 15:56:03 +0800

    Update hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,61 +1,65 @@
-local spaces = require("hs.spaces") -- https://github.com/asmagill/hs._asm.spaces
+local spaces = require("hs.spaces")
+local screen = require("hs.screen")
 
--- Switch kitty
-hs.hotkey.bind(
-	_,
-	"f9",
-	function() -- change your own hotkey combo here, available keys could be found here:https://www.hammerspoon.org/docs/hs.hotkey.html#bind
-		local BUNDLE_ID = "net.kovidgoyal.kitty" -- more accurate to avoid mismatching on browser titles
+hs.hotkey.bind({ "command" }, "escape", function()
+	local BUNDLE_ID = "net.kovidgoyal.kitty"
 
-		function getMainWindow(app)
-			-- get main window from app
-			local win = nil
-			while win == nil do
-				win = app:mainWindow()
-			end
-			return win
+	function getMainWindow(app)
+		local win = nil
+		while win == nil do
+			win = app:mainWindow()
+		end
+		return win
+	end
+
+	function moveWindow(kitty, space)
+		local win = getMainWindow(kitty)
+		local isFullScreen = win:isFullScreen()
+
+		if isFullScreen then
+			hs.eventtap.keyStroke("fn", "f", 0, kitty)
 		end
 
-		function moveWindow(kitty, space, mainScreen)
-			-- move to main space
-			local win = getMainWindow(kitty)
-			if win:isFullScreen() then
-				hs.eventtap.keyStroke("fn", "f", 0, kitty)
-			end
-			winFrame = win:frame()
-			scrFrame = mainScreen:fullFrame()
+		winFrame = win:frame()
+		local focusedWin = hs.window.focusedWindow()
+
+		if focusedWin and focusedWin:screen() then
+			local scrFrame = focusedWin:screen():fullFrame()
 			winFrame.w = scrFrame.w
 			winFrame.y = scrFrame.y
 			winFrame.x = scrFrame.x
 			win:setFrame(winFrame, 0)
-			spaces.moveWindowToSpace(win, space)
-			if win:isFullScreen() then
-				hs.eventtap.keyStroke("fn", "f", 0, kitty)
-			end
-			win:focus()
 		end
 
-		local kitty = hs.application.get(BUNDLE_ID)
-		if kitty ~= nil and kitty:isFrontmost() then
-			kitty:hide()
-		else
-			local space = spaces.activeSpaceOnScreen()
-			local mainScreen = hs.screen.mainScreen()
-			if kitty == nil and hs.application.launchOrFocusByBundleID(BUNDLE_ID) then
-				local appWatcher = nil
-				appWatcher = hs.application.watcher.new(function(name, event, app)
-					if event == hs.application.watcher.launched and app:bundleID() == BUNDLE_ID then
-						getMainWindow(app):move(hs.geometry({ x = 0, y = 0, w = 1, h = 1 })) -- move kitty window on top, you could set the window percentage here
-						app:hide()
-						moveWindow(app, space, mainScreen)
-						appWatcher:stop()
-					end
-				end)
-				appWatcher:start()
-			end
-			if kitty ~= nil then
-				moveWindow(kitty, space, mainScreen)
-			end
+		spaces.moveWindowToSpace(win, space)
+
+		if isFullScreen then
+			hs.eventtap.keyStroke("fn", "f", 0, kitty)
+		end
+
+		win:focus()
+	end
+
+	local kitty = hs.application.get(BUNDLE_ID)
+	if kitty ~= nil and kitty:isFrontmost() then
+		kitty:hide()
+	else
+		local space = spaces.activeSpaceOnScreen()
+		local mainScreen = screen.mainScreen()
+		if kitty == nil and hs.application.launchOrFocusByBundleID(BUNDLE_ID) then
+			local appWatcher = nil
+			appWatcher = hs.application.watcher.new(function(name, event, app)
+				if event == hs.application.watcher.launched and app:bundleID() == BUNDLE_ID then
+					getMainWindow(app):move(hs.geometry({ x = 0, y = 0, w = 1, h = 0.4 }))
+					app:hide()
+					moveWindow(app, space)
+					appWatcher:stop()
+				end
+			end)
+			appWatcher:start()
+		end
+		if kitty ~= nil then
+			moveWindow(kitty, space)
 		end
 	end
-)
+end)
