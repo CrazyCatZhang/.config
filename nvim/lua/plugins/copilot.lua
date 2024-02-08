7807b155e5271037aacb36748e75b2daa07d7a37
CrazyCatZhang <crazycatzhang@outlook.com>
2024-02-08 21:25:22 +0800

    Add github copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,15 +1,21 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
+    "github/copilot.vim",
+    config = function()
+      vim.g.copilot_enabled = true
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("n", "<leader>go", ":Copilot<CR>", { silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>gE", ":Copilot enable<CR>", { silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>gD", ":Copilot disable<CR>", { silent = true })
+      vim.api.nvim_set_keymap("i", "<c-p>", "<Plug>(copilot-suggest)", { noremap = true })
+      vim.api.nvim_set_keymap("i", "<c-n>", "<Plug>(copilot-next)", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("i", "<c-l>", "<Plug>(copilot-previous)", { noremap = true, silent = true })
+      vim.cmd('imap <silent><script><expr> <C-C> copilot#Accept("")')
+      vim.cmd([[
+			let g:copilot_filetypes = {
+	       \ 'TelescopePrompt': v:false,
+	     \ }
+			]])
+    end,
   },
 }
