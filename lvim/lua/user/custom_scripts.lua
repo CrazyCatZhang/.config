1a594d568c7f7df9d6fa3099d4cdfa564e87e710
CrazyCatZhang <crazycatzhang@outlook.com>
2024-02-26 16:59:34 +0800

    Add toggle words scripts

--- lvim/lua/user/custom_scripts.lua
+++ lvim/lua/user/custom_scripts.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.config = function()
+M.toggleWindowMaxsize = function()
   window_sizes = window_sizes or {}
 
   function toggle_maximize_split()
@@ -25,6 +25,45 @@ M.config = function()
   end
 
   vim.api.nvim_set_keymap("n", "<C-f>", ":lua toggle_maximize_split()<CR>", { noremap = true, silent = true })
+end
+
+M.toggleWords = function()
+  function toggle_words()
+    local word_under_cursor = vim.fn.expand "<cword>"
+    local opposites = {
+      ["true"] = "false",
+      ["false"] = "true",
+      ["yes"] = "no",
+      ["no"] = "yes",
+      ["on"] = "off",
+      ["off"] = "on",
+      ["enable"] = "disable",
+      ["disable"] = "enable",
+      ["enabled"] = "disabled",
+      ["disabled"] = "enabled",
+      ["visible"] = "hidden",
+      ["hidden"] = "visible",
+      ["public"] = "private",
+      ["private"] = "public",
+      ["static"] = "dynamic",
+      ["dynamic"] = "static",
+      ["const"] = "var",
+      ["var"] = "const",
+      ["let"] = "const",
+    }
+
+    local new_word = opposites[word_under_cursor]
+    if new_word then
+      local keys = vim.api.nvim_replace_termcodes("ciw" .. new_word .. "<Esc>", true, false, true)
+      vim.api.nvim_feedkeys(keys, "n", false)
+    end
+  end
+  vim.api.nvim_set_keymap("n", "tw", "<cmd>lua toggle_words()<CR>", { noremap = true, silent = true })
+end
+
+M.config = function()
+  M.toggleWindowMaxsize()
+  M.toggleWords()
 end
 
 return M
