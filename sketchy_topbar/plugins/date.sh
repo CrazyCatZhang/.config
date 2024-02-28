a01af79a4a5ac9a1cd1f1c2f288e6b5153c1c0b7
CrazyCatZhang <crazycatzhang@outlook.com>
2024-02-29 02:52:45 +0800

    Fix some topbar issue

--- sketchy_topbar/plugins/date.sh
+++ sketchy_topbar/plugins/date.sh
@@ -3,5 +3,12 @@
 # The $NAME variable is passed from sketchy_topbar and holds the name of
 # the item invoking this script:
 # https://felixkratz.github.io/sketchy_topbar/config/events#events-and-scripting
+#
 
-sketchy_topbar --set $NAME label="$(date '+%a %b %d')"
+if [ -z "$NAME" ]; then
+  echo "Error: NAME variable is not set."
+  exit 1
+fi
+
+DATE_LABEL=$(date '+%a %b %d')
+sketchy_topbar --set "$NAME" "label=$DATE_LABEL"
--- sketchy_topbar/plugins/spotify.sh
+++ sketchy_topbar/plugins/spotify.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 status() {
 	if osascript -e 'tell application "Spotify"
     set playerState to player state as string
@@ -9,10 +11,6 @@ playerState
 		echo "paused"
 	fi
 }
-
-BACK_SCRIPT="cd ~/.config/sketchy_topbar/spotify && ./back.sh"
-PAUSE_PLAY_SCRIPT="cd ~/.config/sketchy_topbar/spotify/ && ./playpause.sh && source '$PLUGIN_DIR/spotify.sh'"
-FORWARD_SCRIPT="cd ~/.config/sketchy_topbar/spotify && ./forward.sh"
 
 if [ $(status) = "playing" ]; then
 	sketchy_topbar --set pause icon=''
