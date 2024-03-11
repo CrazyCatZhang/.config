867c430cdcd7cc6f6f3646446b3c660c87ec4dd8
CrazyCatZhang <crazycatzhang@outlook.com>
2024-03-11 18:35:25 +0800

    Reset playing location

--- sketchy_topbar/items/playing.sh
+++ sketchy_topbar/items/playing.sh
@@ -1,15 +1,22 @@
-sketchy_topbar --add item line right \
-	--set line icon="" \
-	icon.color=0xff9fd06a \
-	icon.font="MonaspiceRn Nerd Font Propo:Regular:14.8" \
-	\
-	--add item playing right \
-	--set playing update_freq=5 \
-	icon.color=0xff9fd06a \
-	label.font="MonaspiceRn Nerd Font Propo:Bold Italic:12.0" \
-	label.padding_right=5 \
-	label.color=$COLOR_TWO \
-	script="$PLUGIN_DIR/playing.sh" \
-	label.y_offset=-1 \
-	background.padding_right=3 \
-	icon.font="MonaspiceRn Nerd Font Propo:Regular:14.8"
+#!/bin/sh
+
+sketchy_topbar --add item line left \
+  --set line icon="" \
+  icon.color=0xff9fd06a \
+  icon.font="MonaspiceRn Nerd Font Propo:Regular:14.8" \
+  \
+  --add item playing left \
+  --set playing update_freq=5 \
+  label.font="MonaspiceRn Nerd Font Propo:Bold Italic:12.0" \
+  label.padding_left=-15 \
+  label.color=$COLOR_TWO \
+  script="$PLUGIN_DIR/playing.sh" \
+  label.y_offset=-1 \
+  background.padding_left=3 \
+  \
+  --add item music left \
+  --set music icon="󰎇" \
+  icon.color=0xff9fd06a \
+  icon.y_offset=-1 \
+  icon.font="MonaspiceRn Nerd Font Propo:Regular:14.8"
+
